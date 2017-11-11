afc3b4bb7a04eb9a448d1e58e8441611b5bb8d6b
jaceksen <jaceksen@gmail.com>
2017-11-11 20:07:44 +0100

    lekcja 19 styles

---
+++ src/app/components/sandbox/sandbox.componet.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector:'sandbox',
+    templateUrl:'./sandbox.component.html',
+    styleUrls:['./sandbox.component.css']
+//     styles: [`
+//     .special{
+//         color:green;
+//         font-size: 20px;
+//         text-transform:uppercase;
+//     }
+// `]
+})
+
+export class SandboxComponent{
+    name:string='aaa';
+}
