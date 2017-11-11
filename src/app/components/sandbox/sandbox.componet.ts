2ae879b18e80d17b220fadf5712115d00e23ece9
jaceksen <jaceksen@gmail.com>
2017-11-11 23:14:32 +0100

    lekcja20_arrays

--- src/app/components/sandbox/sandbox.componet.ts
+++ src/app/components/sandbox/sandbox.componet.ts
@@ -2,17 +2,44 @@ import { Component } from '@angular/core';
 
 @Component({
     selector:'sandbox',
-    templateUrl:'./sandbox.component.html',
-    styleUrls:['./sandbox.component.css']
-//     styles: [`
-//     .special{
-//         color:green;
-//         font-size: 20px;
-//         text-transform:uppercase;
-//     }
-// `]
+    template:`
+        {{ people }}
+
+        <ul>
+            <li *ngFor="let person of people; let i = index">
+            {{person}}.{{i+1}}
+            </li>
+        </ul>
+        <ul>
+        <li *ngFor="let person of people2; let i = index">
+        {{person.firstName}} {{person.lastName}}
+        </li>
+    </ul>
+    `
 })
 
 export class SandboxComponent{
     name:string='aaa';
+
+    people = ['Rick', 'Daryl', 'Carl', 'Glen'];
+
+    people2=[
+        {
+            firstName: 'Jacek',
+            lastName: 'Seń'
+        },
+        {
+            firstName: 'Rima',
+            lastName: 'Avetyan'
+        },
+        {
+            firstName: 'Maja',
+            lastName: 'Seń'
+        }
+    ];
+
+    constructor(){
+        this.people[2] = 'JJJJJJJ';
+        this.people2[0].firstName = 'JACEK';
+    }
 }
