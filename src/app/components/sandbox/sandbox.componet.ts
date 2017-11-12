5535f5a1c75046533c408a4f3470d2b307d99d65
jaceksen <jaceksen@gmail.com>
2017-11-12 13:38:05 +0100

    lekcja31_formSubmit

--- src/app/components/sandbox/sandbox.componet.ts
+++ src/app/components/sandbox/sandbox.componet.ts
@@ -4,28 +4,33 @@ import { Component } from '@angular/core';
     selector:'sandbox',
     template:`
 
-    <div class="container">
-    <form>
-        <div class="form-group">
-        <label>Name</label>
-        <input type="text" [(ngModel)]="name" name="name" class="form-control">
-        </div>
-        <div class="form-group">
-        <label>Age</label>
-        <input type="number" [(ngModel)]="age" name="age" class="form-control">
-        </div>
-        <input type="submit" value="Submit" class="btn btn-success">
-    </form>
+        <div class="container">
 
-    <h4>Name: {{name}}</h4>
-    <h4>Age: {{age}}</h4>
-    </div>
+        <form (submit)="onSubmit()">
+         <div class="form-group">
+            <label>Name</label>
+            <input type="text" class="form-control" [(ngModel)]="name" name="name">
+         </div>
+         <input type="submit" class="btn btn-success" value="Submit">
+         </form>
+         <hr>
+         <ul class="list-group">
+            <li class="list-group-item" *ngFor="let user of users">{{ user }}</li>
+        </ul>
+         </div>
 
         `
 })
 
 export class SandboxComponent{
-    
+
     name:string;
-    age:number = 0;
+    users:string[] = ['Jacek Seń','Rima Avetyan','Maja Seń'];
+
+    onSubmit(){
+        console.log(this.name);
+        this.users.push(this.name);
+        this.name='';
+    }
+
 }
