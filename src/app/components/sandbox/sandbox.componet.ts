8d35a255e015a4e3c3a594485207222f219797aa
Jacek Seń <jaceksen@gmail.com>
2017-11-13 15:12:23 +0100

    lekcja40_http_put_json

--- src/app/components/sandbox/sandbox.componet.ts
+++ src/app/components/sandbox/sandbox.componet.ts
@@ -7,7 +7,7 @@ import { DataService } from '../../services/data.service';
 
         <div class="container">
 
-        <form (submit)="onSubmit()">
+        <form (submit)="onSubmit(isEdit)">
             <div class="form-group">
                 <label>Name</label>
                 <input type="text" class="form-control" [(ngModel)]="user.name" name="name"/>
@@ -33,6 +33,7 @@ import { DataService } from '../../services/data.service';
                     <li class="list-group-item list-group-item-success">Email: {{user.email}}</li>
                     <li class="list-group-item list-group-item-warning">Phone: {{user.phone}}</li>
                 </ul>
+                <button class="btn btn-primary btn-sm" (click)="onEditClick(user)">Edit</button>
                 <button class="btn btn-danger btn-sm" (click)="onDeleteClick(user.id)">Delete</button>
             </div>
             
@@ -47,10 +48,12 @@ export class SandboxComponent{
 
     users:any[];
     user = {
+        id:'',
         name:'',
         email:'',
         phone:''
     };
+    isEdit:boolean = false;
 
     constructor(public dataService:DataService){
         this.dataService.getUsers().subscribe(users => {
@@ -59,11 +62,22 @@ export class SandboxComponent{
         });
     }
 
-    onSubmit(){
-        this.dataService.addUser(this.user).subscribe(user => {
-            console.log(user);
-            this.users.unshift(user);
-        });
+    onSubmit(isEdit){
+        if(isEdit){
+            this.dataService.updateUser(this.user).subscribe(user => {
+                for(let i=0; i<this.users.length; i++){
+                    if(this.users[i].id == this.user.id){
+                        this.users.splice(i,1);
+                    }
+                }
+                this.users.unshift(this.user);
+            });
+        } else {
+            this.dataService.addUser(this.user).subscribe(user => {
+                console.log(user);
+                this.users.unshift(user);
+            });
+        }
     }
 
     onDeleteClick(id){
@@ -76,6 +90,11 @@ export class SandboxComponent{
                 }
             }
         });
+    }
+
+    onEditClick(user){
+        this.isEdit = true;
+        this.user = user;
     }
 
 
