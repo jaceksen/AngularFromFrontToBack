26f178071c1908eb17065ca97087f221100279f2
Jacek Seń <jaceksen@gmail.com>
2017-11-13 14:31:33 +0100

    lekcja38_http_post_json

--- src/app/components/sandbox/sandbox.componet.ts
+++ src/app/components/sandbox/sandbox.componet.ts
@@ -6,6 +6,26 @@ import { DataService } from '../../services/data.service';
     template:`
 
         <div class="container">
+
+        <form (submit)="onSubmit()">
+            <div class="form-group">
+                <label>Name</label>
+                <input type="text" class="form-control" [(ngModel)]="user.name" name="name"/>
+            </div>
+            <div class="form-group">
+                <label>Email</label>
+                <input type="text" class="form-control" [(ngModel)]="user.email" name="email"/>
+            </div>
+            <div class="form-group">
+                <label>Phone</label>
+                <input type="text" class="form-control" [(ngModel)]="user.phone" name="phone"/>
+            </div>
+            <input type="submit" class="btn btn-success" value="Submit"/>
+        </form>
+
+        <br><hr><br>
+
+
         <div *ngFor="let user of users">
             <div class="well">
                 <ul class="list-group">
@@ -24,11 +44,23 @@ import { DataService } from '../../services/data.service';
 export class SandboxComponent{
 
     users:any[];
+    user = {
+        name:'',
+        email:'',
+        phone:''
+    };
 
     constructor(public dataService:DataService){
         this.dataService.getUsers().subscribe(users => {
             //console.log(users);
             this.users = users;
+        });
+    }
+
+    onSubmit(){
+        this.dataService.addUser(this.user).subscribe(user => {
+            console.log(user);
+            this.users.unshift(user);
         });
     }
 
--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -14,4 +14,9 @@ export class DataService{
             .map(res => res.json());
     }
 
+    addUser(user){
+        return this.http.post('https://jsonplaceholder.typicode.com/users', user)
+            .map(res => res.json())
+    }
+
 }
