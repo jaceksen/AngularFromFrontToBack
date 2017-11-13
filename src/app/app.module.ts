f773c100e49269d5b95e978fa2ae4dcfb913b49e
Jacek Seń <jaceksen@gmail.com>
2017-11-13 16:30:20 +0100

    lekcja43_Router_Navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
 
 
 import { AppComponent } from './app.component';
 import { SandboxComponent } from './components/sandbox/sandbox.componet';
-import { DataService } from './services/data.service';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+
+const appRoutes: Routes = [
+  {path:'', component:HomeComponent},
+  {path:'about', component:AboutComponent}
+];
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    SandboxComponent
+    SandboxComponent,
+    HomeComponent,
+    AboutComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule,
-    HttpModule
+    RouterModule.forRoot(appRoutes)
   ],
-  providers: [DataService],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/components/sandbox/sandbox.componet.ts
+++ src/app/components/sandbox/sandbox.componet.ts
@@ -1,101 +1,13 @@
 import { Component } from '@angular/core';
-import { DataService } from '../../services/data.service';
 
 @Component({
     selector:'sandbox',
     template:`
 
-        <div class="container">
-
-        <form (submit)="onSubmit(isEdit)">
-            <div class="form-group">
-                <label>Name</label>
-                <input type="text" class="form-control" [(ngModel)]="user.name" name="name"/>
-            </div>
-            <div class="form-group">
-                <label>Email</label>
-                <input type="text" class="form-control" [(ngModel)]="user.email" name="email"/>
-            </div>
-            <div class="form-group">
-                <label>Phone</label>
-                <input type="text" class="form-control" [(ngModel)]="user.phone" name="phone"/>
-            </div>
-            <input type="submit" class="btn btn-success" value="Submit"/>
-        </form>
-
-        <br><hr><br>
-
-
-        <div *ngFor="let user of users">
-            <div class="well">
-                <ul class="list-group">
-                    <li class="list-group-item list-group-item-success">Name: {{user.name}}</li>
-                    <li class="list-group-item list-group-item-success">Email: {{user.email}}</li>
-                    <li class="list-group-item list-group-item-warning">Phone: {{user.phone}}</li>
-                </ul>
-                <button class="btn btn-primary btn-sm" (click)="onEditClick(user)">Edit</button>
-                <button class="btn btn-danger btn-sm" (click)="onDeleteClick(user.id)">Delete</button>
-            </div>
-            
-            <br>
-        </div>
-        </div>
-
         `
 })
 
 export class SandboxComponent{
-
-    users:any[];
-    user = {
-        id:'',
-        name:'',
-        email:'',
-        phone:''
-    };
-    isEdit:boolean = false;
-
-    constructor(public dataService:DataService){
-        this.dataService.getUsers().subscribe(users => {
-            //console.log(users);
-            this.users = users;
-        });
-    }
-
-    onSubmit(isEdit){
-        if(isEdit){
-            this.dataService.updateUser(this.user).subscribe(user => {
-                for(let i=0; i<this.users.length; i++){
-                    if(this.users[i].id == this.user.id){
-                        this.users.splice(i,1);
-                    }
-                }
-                this.users.unshift(this.user);
-            });
-        } else {
-            this.dataService.addUser(this.user).subscribe(user => {
-                console.log(user);
-                this.users.unshift(user);
-            });
-        }
-    }
-
-    onDeleteClick(id){
-        console.log(id);
-        this.dataService.deleteUser(id).subscribe(res => {
-            console.log(res);
-            for(let i=0; i<this.users.length; i++){
-                if(this.users[i].id == id){
-                    this.users.splice(i,1);
-                }
-            }
-        });
-    }
-
-    onEditClick(user){
-        this.isEdit = true;
-        this.user = user;
-    }
 
 
 }
